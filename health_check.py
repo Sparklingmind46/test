dc3a24c3f0eb1ed90a8ebabaa42652a63b5665ae
Sparklingmind46 <sparklingmind42@gmail.com>
2024-11-08 20:12:18 +0530

    Add files via upload

---
+++ health_check.py
@@ -0,0 +1,11 @@
+# health_check.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Health check passed!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
